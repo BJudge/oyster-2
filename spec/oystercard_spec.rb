d62ca04ff158ec0c1f2f6809f5b08e76da4fbe39
BartJudge <BartJudge@gmail.com>
2016-10-02 14:16:11 +0100

    deduct method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,7 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
     describe '#top_up' do
-      it { is_expected.to respond_to(:top_up).with(1).argument }
+      # it { is_expected.to respond_to(:top_up).with(1).argument }
 
       it 'can top up the balance' do
         expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
@@ -15,7 +15,15 @@ describe Oystercard do
         maximum_balance = Oystercard::MAXIMUM_BALANCE
         subject.top_up(maximum_balance)
         expect{ subject.top_up 1 }.to raise_error "Maximum balance of £'#{maximum_balance}' exceeded"
+      end
+    end
 
+    describe '#deduct' do
+      # it { is_expected.to respond_to(:deduct).with(1).argument}
+
+      it 'can reduce money from the balance' do
+        subject.top_up(20)
+        expect{ subject.deduct 3 }.to change{ subject.balance }.by -3
       end
     end
 
