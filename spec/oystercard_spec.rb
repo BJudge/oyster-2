ec9f4b6ba517778d979ffc55ef6866ecbd2e1819
BartJudge <BartJudge@gmail.com>
2016-10-02 13:50:56 +0100

    maximum balance clause added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,13 @@ describe Oystercard do
       it { is_expected.to respond_to(:top_up).with(1).argument }
 
       it 'can top up the balance' do
-        expect{ subject.top_up 1 }.to change{ subject.balance }.by 1 
+        expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      end
+
+      it 'has a maximum defualt capacity of £90' do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        subject.top_up(maximum_balance)
+        expect{ subject.top_up 1 }.to raise_error "Maximum balance of £'#{maximum_balance}' exceeded"
 
       end
     end
