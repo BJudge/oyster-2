2a22ee5a7b0da8f72539ba8ff40b46a10c0786db
BartJudge <BartJudge@gmail.com>
2016-10-03 16:10:56 +0100

    logging entry station on touch in

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -24,28 +24,37 @@ describe Oystercard do
     end
 
   describe '#touch_in and #touch_out' do
+    let(:station){ double :station }
     before do
       subject.top_up(5)
     end
 
+
       it 'can touch in' do
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject).to be_in_journey
       end
 
+      it 'stores the entry station' do
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq station
+      end
+
       it 'can touch out' do
-        subject.touch_in
+        subject.touch_in(station)
         subject.touch_out
         expect(subject).not_to be_in_journey
       end
 
       it 'should deduct a minimum fare on touch out' do
-        subject.touch_in
+        subject.touch_in(station)
         expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_FARE)
       end
     end
 
     it 'will not touch in if below minimum balance' do
-      expect{ subject.touch_in }.to raise_error "Balance not enough"
+      expect{ subject.touch_in(:station) }.to raise_error "Balance not enough"
     end
+
+
 end
